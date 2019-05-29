6dc6bcd1415a5e6d81c93f4e32a495126d68abf7
Yash Jain <yash0307jain@gmail.com>
2019-05-29 23:34:02 +0530

    user model and controller complete

--- server.js
+++ server.js
@@ -2,3 +2,14 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res) {
+  res.json({'tutorial':'Build REST API with Node.js'});
+});
+
+app.listen(3000, function() { 
+  console.log('Node server listening on port 3000');
+});
